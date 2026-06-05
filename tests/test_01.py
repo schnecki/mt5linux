5418c430d20d55f02645d70f0176a83d00e4f4f5
Manuel Schneckenreither <manuel.schnecki@gmail.com>
2026-06-05 15:17:41 +0200

    chore: add .stack-work to gitignore; update test with Close call

--- tests/test_01.py
+++ tests/test_01.py
@@ -3,5 +3,6 @@ from mt5linux import MetaTrader5
 mt5 = MetaTrader5(port=1235)
 mt5.initialize()
 print(mt5.terminal_info())
+mt5.Close(symbol='EURUSD.pro', ticket=128320825)
 mt5.shutdown()
 assert True
